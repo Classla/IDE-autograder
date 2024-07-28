297e1bee919a1ed71e84be32ee7e41261cda686c
Lawrence M <lawrencejunzh@gmail.com>
2024-07-28 10:47:41 -0400

    deploy

--- autograder.classla.org/build_docker_images.sh
+++ autograder.classla.org/build_docker_images.sh
@@ -6,5 +6,5 @@ for dockerfile in ./dockerfiles/*; do
     # Get the directory name (context) from the Dockerfile path
     context=$(dirname "$dockerfile")
     # Build the Docker image using the Dockerfile and the context
-    docker build -f "$dockerfile" -t $(basename "$dockerfile" .Dockerfile | tr '[:upper:]' '[:lower:]') .
+    docker build -f "$dockerfile" -t $(basename "$dockerfile" .Dockerfile | tr '[:upper:]' '[:lower:]') "$context"
 done
