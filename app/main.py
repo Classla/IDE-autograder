77ff2c48ed64a43d3a4af35cb7e33dc5ed7b096d
Camden Spehl <47334268+CamdenSpehl@users.noreply.github.com>
2024-09-27 18:20:24 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ app: FastAPI = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Adjust this to match your localhost port
+    allow_origins=["*"],  # Adjust this to match your localhost port
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
